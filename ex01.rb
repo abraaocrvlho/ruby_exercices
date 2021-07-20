abb215950c4baaf4f2f9bc86c39646fa1fe7724e
abraaocrvlho <abraaocrvlho@gmail.com>
2021-07-20 18:01:05 -0300

    Some changes.

--- ex01.rb
+++ ex01.rb
@@ -1,5 +1,7 @@
 # Write a Ruby program that displays the message "Hello World, Rubysts :)"
 
+# Displaying "Hello World, Rubysts" with Print
 print 'Hello World, Rubysts :)'
 
+# Showing "Hello World, Rubysts" with Puts
 puts "Hello World, Rubysts :)"
--- ex02.rb
+++ ex02.rb
@@ -4,6 +4,6 @@ print 'Enter your name: '
 name = gets.chomp
 
 print 'Enter your age: '
-age = gets.chomp.to_i
+age = gets.chomp.to_i # .to_i transforms strings into integers
 
 puts "Your name is #{name} and your age is #{age} years old."
